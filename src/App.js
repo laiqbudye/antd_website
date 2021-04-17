790b0552c191854f2b0d8975de83d17922ab4647
laiqb <laiqbudye@gmail.com>
2021-04-18 03:15:45 +0530

    Header || implemented the Header part

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import React from 'react';
 import './App.css';
+import 'antd/dist/antd.css';
+import Appheader from './components/common/Appheader';
+import { Layout } from 'antd';
+const { Header } = Layout;
+
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Layout className="mainLayout">
+    <Header>
+      <Appheader />
+    </Header>
+  </Layout>
   );
 }
 
