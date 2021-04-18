aa84270628efb5632c4eb0c3fa8d5616696ee86b
laiqb <laiqbudye@gmail.com>
2021-04-18 19:54:49 +0530

    Features || added features section

--- src/views/Apphome.js
+++ src/views/Apphome.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Apphero from '../components/home/Apphero';
 import Appabout from '../components/home/Appabout';
+import Appfeature from '../components/home/Appfeature';
 
 const Apphome = () => {
     return (
         <div className='main'>
             <Apphero />
             <Appabout />
+            <Appfeature />
         </div>
     )
 }
