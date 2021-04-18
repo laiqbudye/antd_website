0fea9414f443479f87aac17a6fb6f78067bfa42d
laiqb <laiqbudye@gmail.com>
2021-04-18 18:54:30 +0530

    About || added about section

--- src/views/Apphome.js
+++ src/views/Apphome.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Apphero from '../components/home/Apphero';
+import Appabout from '../components/home/Appabout';
 
 const Apphome = () => {
     return (
         <div className='main'>
             <Apphero />
+            <Appabout />
         </div>
     )
 }
